eb55e5ce4baf5926cad7cf274eb17e05cc9e9429
Ivan Sudar <ivan.sudar12@gmail.com>
2019-01-31 18:31:55 +0100

    finishid

--- app/src/main/java/com/example/sudo/hardtogetup/adapters/DrawerRecyclerAdapter.java
+++ app/src/main/java/com/example/sudo/hardtogetup/adapters/DrawerRecyclerAdapter.java
@@ -1,6 +1,7 @@
 package com.example.sudo.hardtogetup.adapters;
 
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,13 +9,18 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.sudo.hardtogetup.R;
+import com.example.sudo.hardtogetup.models.Alarm;
 import com.example.sudo.hardtogetup.models.MathProblem;
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class DrawerRecyclerAdapter extends RecyclerView.Adapter<DrawerRecyclerAdapter.ViewHolder> {
 
@@ -43,12 +49,37 @@ public class DrawerRecyclerAdapter extends RecyclerView.Adapter<DrawerRecyclerAd
         notifyDataSetChanged();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnLongClickListener {
         @BindView(R.id.tvMathProblem)
         TextView tvMathProblem;
+        @BindView(R.id.clDrawerItem)
+        ConstraintLayout clDrawerItem;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
+            clDrawerItem.setOnLongClickListener(this);
+        }
+
+        @Override
+        public boolean onLongClick(View view) {
+            //brisanje alarma iz liste moramo obrisati i iz realma
+            if (getAdapterPosition() > 3) {
+
+                String problem = mathProblemList.get(getAdapterPosition()).getProblem();
+                mathProblemList.remove(getAdapterPosition());
+                notifyItemRemoved(getAdapterPosition());
+                notifyItemRangeChanged(getAdapterPosition(), mathProblemList.size());
+                Realm realm = Realm.getDefaultInstance();
+                realm.executeTransaction(new Realm.Transaction() {
+                    @Override
+                    public void execute(Realm realm) {
+                        RealmResults<MathProblem> alarm = realm.where(MathProblem.class).equalTo("problem", problem).findAll();
+                        alarm.deleteAllFromRealm();
+                    }
+                });
+            }
+            return false;
         }
     }
 }
